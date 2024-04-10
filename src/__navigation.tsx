ad865f49f03ea209757ad11d2ffa9783ae54e704
Remi <remitache@gmail.com>
2024-04-10 23:56:28 +0200

    :bug:

--- src/__navigation.tsx
+++ src/__navigation.tsx
@@ -1,29 +1,29 @@
-import { Pathnames, createSharedPathnamesNavigation } from 'next-intl/navigation';
+// import { Pathnames, createSharedPathnamesNavigation } from 'next-intl/navigation';
 
-export const locales = ['en', 'fr'];
-export const localePrefix = 'always';
+// export const locales = ['en', 'fr'];
+// export const localePrefix = 'always';
 
-// The `pathnames` object holds pairs of internal
-// and external paths, separated by locale.
-export const pathnames = {
-  // If all locales use the same pathname, a
-  // single external path can be provided.
-  '/': '/',
-  '/blog': '/blog',
+// // The `pathnames` object holds pairs of internal
+// // and external paths, separated by locale.
+// export const pathnames = {
+//   // If all locales use the same pathname, a
+//   // single external path can be provided.
+//   '/': '/',
+//   '/blog': '/blog',
  
-  // If locales use different paths, you can
-  // specify each external path per locale.
-  '/about-us': {
-    en: '/en/about-us',
-    fr: '/fr/about-us'
-  },
-  '/service': {
-    en: '/en/service',
-    fr: '/fr/service'
-  }
-} satisfies Pathnames<typeof locales>;
+//   // If locales use different paths, you can
+//   // specify each external path per locale.
+//   '/about-us': {
+//     en: '/en/about-us',
+//     fr: '/fr/about-us'
+//   },
+//   '/service': {
+//     en: '/en/service',
+//     fr: '/fr/service'
+//   }
+// } satisfies Pathnames<typeof locales>;
 
 
 
-export const { Link, redirect, usePathname, useRouter } =
-  createSharedPathnamesNavigation({ locales, localePrefix });
+// export const { Link, redirect, usePathname, useRouter } =
+//   createSharedPathnamesNavigation({ locales, localePrefix });
