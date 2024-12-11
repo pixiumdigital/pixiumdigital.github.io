78eb8837f764399d8363e6d4c0e0a9e5e720b1db
Remi <remitache@gmail.com>
2024-12-11 20:15:52 +0100

    :zap: new build

--- src/app/[locale]/use-case/[slug]/page.tsx
+++ src/app/[locale]/use-case/[slug]/page.tsx
@@ -5,7 +5,7 @@ import { CMS_NAME } from "@/lib/constants";
 import markdownToHtml from "@/lib/markdownToHtml";
 import Alert from "@/app/_components/alert";
 import Container from "@/app/_components/container";
-import Header from "@/app/[locale]/_components/header";
+import Header from "@/app/_components/header";
 import { PostBody } from "@/app/_components/post-body";
 import { PostHeader } from "@/app/_components/post-header";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
