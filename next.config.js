d8a699772a7f77d5bd9cfb5ce8347ccc27e7ae0c
Remi <remitache@gmail.com>
2024-12-16 17:27:55 +0100

    🔗

--- next.config.js
+++ next.config.js
@@ -10,7 +10,7 @@ if(isProd){
     const nextConfig = {
         output: 'export',
         distDir: 'dist',
-        basePath: 'pixiumdigital.com',
+        basePath: '/pixiumdigital.com',
         images:{
             unoptimized:true
         },
