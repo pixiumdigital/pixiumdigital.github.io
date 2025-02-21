196c85be99fd31236da744838c54fe730ca4aa18
Remi <remitache@gmail.com>
2025-02-21 03:33:57 +0100

    :zap:

--- next-sitemap.fr.config.js
+++ next-sitemap.fr.config.js
@@ -137,6 +137,6 @@ module.exports = {
             processedXml: '<?xml-stylesheet type="text/xsl" href="/sitemap/style.xsl"?>'
         }]
     },
-
+    xslUrl: '/sitemap/style.xsl',
     xmlNs: 'xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:image="http://www.google.com/schemas/sitemap-image/1.1"'
 }
